582d0dfbdd16f9a99ae56db62895b268148a829c
Pedro Henrique de Mattos Barbosa <133368598+phmbarbosa@users.noreply.github.com>
2025-03-10 14:54:36 -0300

    Create 1.py

---
+++ Exercicio4/1.py
@@ -0,0 +1,5 @@
+numeros = list(map(int, input("Digite uma lista de números inteiros separados por espaço: ").split()))
+soma = sum(numeros)
+print(f"A soma dos elementos da lista é: {soma}")
+
+#acho q n ta certo n, verificar na aula dps
