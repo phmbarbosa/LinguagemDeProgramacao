b1e38f36b194188a70b64dfe21166f5c73df4dd7
Pedro Henrique de Mattos Barbosa <133368598+phmbarbosa@users.noreply.github.com>
2025-03-10 19:29:29 -0300

    Create 2.py

---
+++ Exercicio5/2.py
@@ -0,0 +1,13 @@
+def eh_primo(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+numero = int(input("Digite um número para verificar se é primo: "))
+if eh_primo(numero):
+    print(f"{numero} é um número primo.")
+else:
+    print(f"{numero} não é um número primo.")
