dd20a319d0ead7040e7b1436a9dca189ff7c30cb
Pedro Henrique de Mattos Barbosa <133368598+phmbarbosa@users.noreply.github.com>
2025-03-21 11:57:54 -0300

    Update 3.py

--- Exercicio7/3.py
+++ Exercicio7/3.py
@@ -5,3 +5,9 @@ dicionario = {
     "amor": "love",
     "feliz": "happy"
 }
+
+palavra_pt = input("Digite uma palavra em português: ")
+if palavra_pt in dicionario:
+    print(f"A tradução de {palavra_pt} é {dicionario[palavra_pt]}.")
+else:
+    print("Tradução não encontrada.")
