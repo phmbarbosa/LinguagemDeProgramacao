82e5d3fcd3743012016a931f9575fa1d35a1425b
Pedro Henrique de Mattos Barbosa <133368598+phmbarbosa@users.noreply.github.com>
2025-04-11 12:27:01 -0300

    Create 2.py

---
+++ Curso2/Modulo1/2.py
@@ -0,0 +1,11 @@
+import platform
+
+
+so = platform.system()
+versao = platform.version()
+arquitetura = platform.architecture()[0]
+
+print("Informações do sistema:")
+print(f"Sistema Operacional: {so}")
+print(f"Versão: {versao}")
+print(f"Arquitetura: {arquitetura}")
