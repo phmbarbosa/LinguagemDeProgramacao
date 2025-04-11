c6ca3dd917e8d17b8bbddf9092a56ed05f9f8c37
Pedro Henrique de Mattos Barbosa <133368598+phmbarbosa@users.noreply.github.com>
2025-04-11 11:40:13 -0300

    Create 1.py

---
+++ Curso2/1.py
@@ -0,0 +1,10 @@
+import math
+import random
+
+numero = float(input("Digite um número para calcular a raiz quadrada: "))
+
+raiz = math.sqrt(numero)
+print(f"A raiz quadrada de {numero} é {raiz:.2f}")
+
+aleatorio = random.randint(1, 100)
+print(f"Número aleatório gerado entre 1 e 100: {aleatorio}")
